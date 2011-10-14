956d9ad7dcd261ed9cf3cf3cf443dd396ddac467
Kailash Nadh <kailash.nadh@gmail.com>
2011-10-14 11:18:00 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# tinytabs
+tinytabs is a tiny (1.1 KB minified) tabbing plugin for jQuery. All you need is a few layers in a container layer, and bam, tabbed interface. 
+
+Kailash Nadh, October 2011
+License:	MIT License
+Documentation and Demo: http://kailashnadh.name/code/tinytabs
+
+## Example
+### HTML
+<pre>
+&lt;div id=&quot;mytabs&quot;&gt;
+	&lt;div class=&quot;section&quot; id=&quot;music&quot;&gt;
+		&lt;h3 class=&quot;title&quot;&gt;Music&lt;/h3&gt;
+		Content here
+	&lt;/div&gt;
+
+	&lt;div class=&quot;section&quot; id=&quot;videos&quot;&gt;
+		&lt;h3 class=&quot;title&quot;&gt;Videos&lt;/h3&gt;
+		Content
+	&lt;/div&gt;
+&lt;/div&gt;
+</pre>
+### Javascript
+<pre>
+$(document).ready(function() {
+	$('#mytabs').tinytabs();
+	
+	// or
+	
+	$('#mytabs').tinytabs({		// optional options
+		anchor: false,
+		hide_title: false
+	});
+});
+</pre>
+
+## Options
+<table border="1">
+	<tbody>
+		<tr>
+			<td>anchor</td>
+			<td>
+				<strong><em>true</em> (default) or <em>false</em></strong><br />
+				If enabled, when a tab is clicked, it's id is set in url's hashtag so that the tab
+				is retained on page reloads. Also enables linking to a tab directly.<br />
+				Eg: http://kailashnadh.name/tinytabs#tab-example
+			</td>
+		</tr>
+		<tr>
+			<td>hide_title</td>
+			<td>
+				<strong><em>true</em> (default) or <em>false</em></strong><br />
+				Hide the title element within section elements.
+			</td>
+		</tr>
+		<tr>
+			<td>section_class</td>
+			<td>
+				Section element's class. Default is <strong><em>section</em></strong>.
+			</td>
+		</tr>
+		<tr>
+			<td>tabs_class</td>
+			<td>
+				Tab (ul) container's class. Default is <strong><em>tabs</em></strong>.
+			</td>
+		</tr>
+		<tr>
+			<td>tab_class</td>
+			<td>
+				Individual tab's (li) class. Default is <strong><em>tab</em></strong>.
+			</td>
+		</tr>
+		<tr>
+			<td>title_class</td>
+			<td>
+				Title element's tag. Default is <strong><em>title</em></strong>.
+			</td>
+		</tr>
+	</tbody>
+</table>
---
+++ tinytabs.js
@@ -0,0 +1,104 @@
+/*
+	Kailash Nadh (http://kailashnadh.name)
+	
+	tinytabs
+	October 2011
+	A tiny tabbed interface plugin for jQuery
+
+	License	:	MIT License
+*/
+(function($) {
+	$.fn.tinytabs = function(new_options) {
+
+		// default options
+		var options = {
+			anchor: true,
+			hide_title: true,
+			section_class: 'section',
+			tabs_class: 'tabs',
+			tab_class: 'tab',
+			title_class: 'title'
+		};
+		var tabs = null,
+			container = this,
+			sections = {};
+
+		if(new_options) {
+			$.extend(options, new_options);
+		}
+		
+		create();
+
+		// ______________________ private methods
+
+		// initialize the tabs
+		function create() {
+			// create tabs container
+			tabs = $('<ul>').attr('class', options.tabs_class);
+			container.addClass('tinytabs').prepend(tabs);
+			
+			// create tabs
+			container.find(' .' + options.section_class).each(function() {
+				var section = $(this),
+					id = $(this).attr('id'),
+					title = $(this).find('.' + options.title_class + ':first');
+
+				if(!id) return true;
+				
+				sections[id] = section;
+
+				// hide title?
+				options.hide_title ? title.hide() : null;
+
+				$(tabs).append(
+					$('<li>').addClass(options.tab_class + ' tab-' + id).append(
+						$('<a>').html( title.html() ).attr('href', '#tab-'+ id ).click(function() {
+							activate(id);
+							return options.anchor;
+						})
+					)
+				);
+			});
+
+			// is anchoring enabled?
+			var href = document.location.hash.replace('#tab-', '');
+			if(options.anchor && href && activate(href)) {
+			} else {
+				for(var id in sections) {
+					activate(id);
+					break;
+				}
+			}			
+		}
+
+		// activate a tab
+		function activate (id) {
+			if(sections[id]) {
+				var section = sections[id];
+			} else {
+				return false;
+			}
+
+			reset();
+
+			tabs.find('.tab-' + id).addClass('sel');
+			sections[id].show();
+			
+			if(options.anchor) {
+				document.location.href = '#tab-' + id;
+			}
+
+			return true;
+		};
+
+		// reset all tabs
+		function reset() {
+			tabs.children().removeClass('sel');
+			$.each(sections, function() {
+				this.hide();
+			});
+		};
+	
+		return this;
+	}
+})(jQuery);
